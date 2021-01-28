3e27ca4a753ab30d7c886683944293a0d60a6609
Amber Lee <lee.amber.alex@gmail.com>
2021-01-28 15:45:47 -0800

    Included function for getting list of users.

---
+++ employee.directory/src/components/utils/API.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const URL = "https://randomuser.me/api/?inc=gender,name,location,email,picture,dob,phone,nat";
+
+
+export default {
+  search: ()=> {
+    return axios.getFullList(URL);
+  }
+};
+
+// https://randomuser.me/documentation#format
+
+// const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
+// const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
+// export default {
+//   search: function(query) {
+//     return axios.get(BASEURL + query + APIKEY);
+//   }
+// };
+
+// $.ajax({
+//   url: 'https://randomuser.me/api/',
+//   dataType: 'json',
+//   success: function(data) {
+//     console.log(data);
+//   }
+// });
+      
