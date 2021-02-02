2f1e21abb27dd7019b3347ac44ef51f7e76ea9fd
Amber Lee <lee.amber.alex@gmail.com>
2021-02-02 13:06:40 -0800

    Using axios instead of fetch.

--- employee.directory/src/components/Page/Home.js
+++ employee.directory/src/components/Page/Home.js
@@ -1,42 +1,53 @@
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
 import Header from "../Header/Header";
-// import API from "../utils/API";
+import API from "../utils/API";
 // import Container from "./components/Container";
 // import SearchForm from "./components/SortSearch";
 // import SearchResults from "./components/SearchResults";
 
 export default function Home() {
-  const [userList, setUserList] = useState();
+  const [userList, setUserList] = useState([]);
 
   useEffect(() => {
-    displayFullList();
-  }, []);
+   API.getFullList().then(res => setUserList(res.results.data)).catch(err => console.log(err));
+   console.log(userList)});
+  
 
-  useEffect(() => {}, [userList]);
+
+
+  // const checkUserState = ()=>{
+  //   if (userList){
+  //     userList.map((userLists) => (
+  //       <ul>
+  //       <li className="list-group-item">{userLists.name}</li>
+  //       </ul>
+  //     ))
+  //   }
+  // }
 
   // const URL =
   //   "https://cors-anywhere.herokuapp.com/randomuser.me/api/?inc=name,nat";
 
-  const URL = "https://cors-anywhere.herokuapp.com/randomuser.me/api/?results=5&inc=name,nat";
+  // const URL =
+  //   "https://cors-anywhere.herokuapp.com/randomuser.me/api/?results=5&inc=name,nat";
 
-  const displayFullList = async () => {
-    const res= await fetch(URL);
-    const userAll = await res.json();
-    console.log(userAll);
-    setUserList(userAll).catch((err) => console.log({ error: err.message }));
-  };
+  // const displayFullList = async () => {
+  //   const res = await fetch(URL);
+  //   const userAll = await res.json();
+  //   console.log(userAll);
+  //   setUserList(userAll).catch((err) => console.log({ error: err.message }));
+  // };
 
   return (
     <div>
       <Header>
         <h1>Employee Directory</h1>
       </Header>
-      <ul>
+      {/* <ul>
         {userList.map((userLists) => (
-          <li className="list-group-item">{userLists.name}</li>
+          <li key={userLists.id} className="list-group-item">{userLists.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
--- employee.directory/src/components/utils/API.js
+++ employee.directory/src/components/utils/API.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const URL = "https://cors-anywhere.herokuapp.com/randomuser.me/api/?inc=gender,name,nat"
+// const URL = "https://cors-anywhere.herokuapp.com/randomuser.me/api/?inc=gender,name,nat"
+
+const URL = "https://cors-anywhere.herokuapp.com/randomuser.me/api/?results=5&inc=name,nat"
 
 const API = {
   getFullList: ()=> {
@@ -9,23 +11,3 @@ const API = {
 };
 export default API;
 
-// https://randomuser.me/documentation#format
-
-// const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-// const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
-
-// Export an object with a "search" method that searches the Giphy API for the passed query
-// export default {
-//   search: function(query) {
-//     return axios.get(BASEURL + query + APIKEY);
-//   }
-// };
-
-// $.ajax({
-//   url: 'https://randomuser.me/api/',
-//   dataType: 'json',
-//   success: function(data) {
-//     console.log(data);
-//   }
-// });
-      
