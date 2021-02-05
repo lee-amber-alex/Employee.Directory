6054e833e2d150214b2f1cd0c976fa013fcdbbc5
Amber Lee <lee.amber.alex@gmail.com>
2021-02-05 13:11:26 -0800

    Centered header.

--- employee.directory/src/components/Header/Header.js
+++ employee.directory/src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 
-
+const styles = {
+  headerTitle: {
+    textAlign: "center"
+  }
+};
 export default function Header(props) {
     return (
 <div className="jumbotron jumbotron-fluid">
-  <div className="container">
+  <div className="container" style={styles.headerTitle}>
     {props.children}
   </div>
 </div>
