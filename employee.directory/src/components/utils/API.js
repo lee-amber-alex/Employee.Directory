b2097ae3141d4c51afd8ba7d12aab1d6f17ea91e
Amber Lee <lee.amber.alex@gmail.com>
2021-02-04 22:02:46 -0800

    Added params to API.

--- employee.directory/src/components/utils/API.js
+++ employee.directory/src/components/utils/API.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-// const URL = "https://cors-anywhere.herokuapp.com/randomuser.me/api/?inc=gender,name,nat"
+// const URL =
+//   "https://cors-anywhere.herokuapp.com/randomuser.me/api/?results=5&inc=picture,name,phone,email,dob,nat=us";
 
-const URL = "https://cors-anywhere.herokuapp.com/randomuser.me/api/?results=5&inc=name,nat"
+const URL = "https://randomuser.me/api/?results=5&inc=picture,name,phone,email,dob,nat=us"
+
+
 
 const API = {
-  getFullList: ()=> {
+  getFullList: () => {
     return axios.get(URL);
-  }
+  },
 };
 export default API;
-
