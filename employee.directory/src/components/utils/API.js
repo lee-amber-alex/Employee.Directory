9cd4dd3d6586cc464296301bba97caa8aabdb721
Amber Lee <lee.amber.alex@gmail.com>
2021-02-05 13:11:00 -0800

    Increased result params.

--- employee.directory/src/components/utils/API.js
+++ employee.directory/src/components/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const URL =
 //   "https://cors-anywhere.herokuapp.com/randomuser.me/api/?results=5&inc=picture,name,phone,email,dob,nat=us";
 
-const URL = "https://randomuser.me/api/?results=5&inc=picture,name,phone,email,dob,nat=us"
+const URL = "https://randomuser.me/api/?results=25&inc=picture,name,phone,email,dob,nat=us"
 
 
 
