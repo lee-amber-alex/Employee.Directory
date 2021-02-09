65034c823f2091d378ede0deab5c93cd36c7cd77
Amber Lee <lee.amber.alex@gmail.com>
2021-02-08 16:50:49 -0800

    Added console logs and additional userstate for sort.

--- employee.directory/src/components/Page/Home.js
+++ employee.directory/src/components/Page/Home.js
@@ -4,19 +4,19 @@ import Table from "../Table";
 import SearchForm from "../SearchForm";
 import API from "../utils/API";
 import Container from "../Container/Container";
-// import Container from "./components/Container";
 
 export default function Home() {
   const [userList, setUserList] = useState([]);
   const [filteredUserlist, setfilteredUserlist] = useState(userList);
-  const [sortedList, setsortedList] = useState([userList]);
   const [name, setName] = useState("");
+  const [sortedList, setsortedList] = useState();
 
   useEffect(() => {
     API.getFullList()
       .then((res) => {
         setUserList(res.data.results);
         setfilteredUserlist(res.data.results);
+        setsortedList(res.data.results);
       })
       .catch((err) => console.log(err));
   }, [setUserList]);
@@ -25,7 +25,6 @@ export default function Home() {
     event.preventDefault();
     const newfilteredUserlist = userList.filter((user) => {
       if (user.name.first.includes(name) || user.name.last.includes(name)) {
-        
         return user;
       }
     });
@@ -33,18 +32,15 @@ export default function Home() {
     setfilteredUserlist(newfilteredUserlist);
   };
 
+    const handleSort = (event) => {
+      event.preventDefault();
+      let sortedData = sortedList.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
+    console.log(sortedList);
+    console.log(sortedData);
+    setsortedList(sortedData);
+    // setfilteredUserlist(sortedData);
+  }
 
-  const handleSort = (event) => { 
-    event.preventDefault();
-    let sortedData = [...sortedList].sort((a,b) => {
-    if(a[name.first] > b[name.first]) return 1;
-    if(a[name.first] < b[name.first]) return -1;
-    return 0;
-  });
-  console.log(sortedList);
-  console.log(sortedData);
-  setsortedList(sortedData)
-}
 
   return (
     <div>
@@ -55,14 +51,11 @@ export default function Home() {
         <SearchForm
           name={name}
           setName={setName}
+          // handleSort={handleSort}
           handleFormSubmit={handleFormSubmit}
         />
-        
-        <Table 
-        rows={filteredUserlist} 
-        handleSort={handleSort}
-       
-        />
+
+        <Table rows={filteredUserlist} handleSort={handleSort} />
       </Container>
     </div>
   );
